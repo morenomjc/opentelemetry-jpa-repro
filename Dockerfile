7c3649d58d63bd1f8f6592e162a9d174cc79ce2a
Mark John Moreno <moreno.mark.john@gmail.com>
2024-04-05 01:27:33 +0800

    add download java agent instead of committing the jar

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM eclipse-temurin:17-jre
 
 ADD target/app.jar /app.jar
-ADD otel-javaagent/opentelemetry-javaagent-2.2.0.jar /opentelemetry-javaagent.jar
+ADD target/agent/opentelemetry-javaagent.jar /opentelemetry-javaagent.jar
 
 ENTRYPOINT java -jar -javaagent:/opentelemetry-javaagent.jar /app.jar
--- src/main/java/com/example/oteljparepro/Application.java
+++ src/main/java/com/example/oteljparepro/Application.java
@@ -1,10 +1,7 @@
 package com.example.oteljparepro;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.OpenTelemetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
